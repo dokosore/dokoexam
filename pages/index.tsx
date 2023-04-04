3f37f3f4924c3801630f4fcef0858436f3f6b6c1
KoshiOsaki <osaki36dg@gmail.com>
2023-04-04 12:36:08 +0900

    大崎が少し編集

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,7 +17,7 @@ export default function Home() {
       <main className={styles.main}>
         <div className={styles.description}>
           <p>
-            Get started by editing&nbsp;
+            大崎が編集しました&nbsp;
             <code className={styles.code}>pages/index.tsx</code>
           </p>
           <div>
